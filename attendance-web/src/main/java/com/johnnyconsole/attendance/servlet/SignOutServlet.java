19439967bf8f0ef6d0f31d311b1c3ea517a75b51
Johnny Console <johnny.console@hotmail.com>
2026-08-11 20:52:36 -0400

    Add sign out servlet

---
+++ attendance-web/src/main/java/com/johnnyconsole/attendance/servlet/SignOutServlet.java
@@ -0,0 +1,23 @@
+package com.johnnyconsole.attendance.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static javax.servlet.http.HttpServletResponse.SC_ACCEPTED;
+
+@WebServlet("/SignOutServlet")
+public class SignOutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(request.getSession() != null) request.getSession().invalidate();
+        request.setAttribute("status", SC_ACCEPTED);
+        request.getRequestDispatcher("/signin.jsp").forward(request, response);
+
+    }
+
+}
