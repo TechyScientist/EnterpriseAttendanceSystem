b6e3f7e28a5b6f0685a219785cdaef9ae829cfea
Johnny Console <johnny.console@hotmail.com>
2026-08-12 14:10:15 -0400

    Finish User persistence object

--- attendance-ejb/src/main/java/com/johnnyconsole/attendance/persistence/User.java
+++ attendance-ejb/src/main/java/com/johnnyconsole/attendance/persistence/User.java
@@ -1,5 +1,31 @@
 package com.johnnyconsole.attendance.persistence;
 
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="attendance_users")
+@NamedQueries({
+        @NamedQuery(name="User.FindByProxData", query="SELECT u FROM User u WHERE u.proxFC = :facilityCode AND u.proxCC = :cardCode"),
+        @NamedQuery(name="User.FindByCredentials", query="SELECT u FROM User u WHERE u.username = :Username")
+})
 public class User {
-    // TODO: Build User persistence object
+    public String lastName, firstName, username, proxFC, proxCC;
+    private String password;
+    public boolean isAdministrator;
+
+    public User(String lastName, String firstName,
+                String username, String password,
+                String proxFC, String proxCC,
+                boolean isAdministrator) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.username = username;
+        this.password = password;
+        this.proxFC = proxFC;
+        this.proxCC = proxCC;
+        this.isAdministrator = isAdministrator;
+    }
 }
