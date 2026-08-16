a88076dba5665b36d06ded0b09f23da75bce145d
Johnny Console <johnny.console@hotmail.com>
2026-08-16 13:34:31 -0400

    Enforce sort order on instructor course list

--- attendance-ejb/src/main/java/com/johnnyconsole/attendance/persistence/Course.java
+++ attendance-ejb/src/main/java/com/johnnyconsole/attendance/persistence/Course.java
@@ -11,7 +11,7 @@ import java.io.Serializable;
         @NamedQuery(name="Course.Lookup", query="SELECT c FROM Course c WHERE UPPER(c.term) = :term AND UPPER(c.subject) = :subject AND c.number = :number AND UPPER(c.section) = :section"),
         @NamedQuery(name="Course.FindByTerm", query="SELECT c FROM Course c WHERE LOWER(c.term) = :term"),
         @NamedQuery(name="Course.FindSectionsByCourseNumber", query="SELECT c FROM Course c WHERE UPPER(c.subject) = :subject AND c.number = :number"),
-        @NamedQuery(name="Course.FindByInstructor", query="SELECT c FROM Course c WHERE c.instructor = :instructor"),
+        @NamedQuery(name="Course.FindByInstructor", query="SELECT c FROM Course c WHERE c.instructor = :instructor ORDER BY c.term DESC, c.subject, c.number, c.section"),
         @NamedQuery(name="Course.FindByName", query="SELECT c FROM Course c WHERE LOWER(c.name) LIKE :name")
 })
 @IdClass(CourseId.class)
